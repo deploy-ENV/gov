1ea4207cb60c02c17b1a7cb19bdad8d84d51297a
noblepaul995 <noblepaul995@gmail.com>
2025-12-13 11:36:56 +0530

    Changes

--- src/components/supplier/pages/Overview.jsx
+++ src/components/supplier/pages/Overview.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ClipboardList, Truck, CreditCard, Home, Calendar, PackageCheck, ArrowRight, X, Package, Archive, Search, Bell, TrendingUp } from "lucide-react";
-
+import Cookies from "js-cookie";
 const stats = [
   {
     label: "Projects Active",
